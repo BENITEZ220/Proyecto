4c50eb34833d13f95f054040fe68c2ccf61e0a8e
BENITEZ220 <a24110220@live.ceti.mx>
2025-06-17 09:26:09 -0600

    V 0.4

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,59 @@
 #include <SFML/Graphics.hpp>
-#include <ground.h>
-#include "obstacle.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 500}), "VIDEOJUEGO");
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 400}), "Video Juego");
+    window.setFramerateLimit(30); // Set the frame rate limit to 60 FPS
 
-    Ground ground;
-    Obstacle obstacle;
+    sf::Texture plant("assets/obstacle.png");
+    sf::Sprite obstacle(plant);
+    obstacle.scale({0.10f, 0.10f});
+    obstacle.setPosition({650.f, 220.f});
 
+    sf::Texture floor("assets/ground.png");
+    sf::Sprite ground(floor);
+    ground.scale({0.30f, 0.30f});
+    ground.setOrigin({0.f, 0.f});
+    ground.setPosition({0.f, 80.f});
+
+    sf::Texture yoshi("assets/dinosaur/dino 1.png");
+    sf::Sprite dinosaur(yoshi);
+    dinosaur.scale({0.20f, 0.20f});
+    dinosaur.setOrigin({0.f, 0.f});
+    dinosaur.setPosition({20.f, 223.f});
+
+    sf::Texture cloud("assets/cloud.png");
+    sf::Sprite sky(cloud);
+    sky.scale({0.30f, 0.30f});
+    sky.setOrigin({0.f, 0.f});
+    sky.setPosition({20.f, 20.f});
+
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.clear(sf::Color::White);
-        ground.draw(window);
-        obstacle.draw(window);
+        // clear the window with black color
+        window.clear(sf::Color(135, 206, 235));
+
+        // draw everything here...
+        // window.draw(...);
+        // inside the main loop, between window.clear() and window.display()
+        window.draw(sky);
+        window.draw(dinosaur);
+        window.draw(obstacle);
+        window.draw(ground);
+
+        // end the current frame
         window.display();
     }
+
+    return 0;
 }
